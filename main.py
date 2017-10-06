ddf4122bb72ec2917f07c811f37186b8fdfc22bb
Andrew Ford <andrewjford@hotmail.co.uk>
2017-10-06 22:00:34 +0100

    Removed convolutions, 3.5-1.0 second runtime

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import time
 import af_model as af
+import numpy as np
 
-structural_homogeneity = .18  # Probability of transverse connections
-dysfunction_parameter = .1  # Fraction of dysfunctional cells
-dysfunction_probability = .1
-substrate_size = (300, 300)
+structural_homogeneity = .1  # Probability of transverse connections
+dysfunction_parameter = .2  # Fraction of dysfunctional cells
+dysfunction_probability = .2
+substrate_size = (200, 200,30)
 pacemaker_period = 220  # pacemaker activation period
 refractory_period = 50
 runtime = 1000
+layer_linkage = .1
 
 start = time.time()
 print('GENERATING SUBSTRATE')
 
 substrate = af.Substrate(substrate_size, structural_homogeneity,
-                         dysfunction_parameter, dysfunction_probability, refractory_period)
+                         dysfunction_parameter, dysfunction_probability, refractory_period, layer_linkage)
 
 print('RUNNING SIMULATION')
 
@@ -22,5 +24,9 @@ results = af.simulation(runtime, pacemaker_period, substrate)
 runtime = time.time() - start
 print('SIMULATION COMPLETE IN {:.1f} SECONDS'.format(runtime))
 
+# np.save('rotor_formation(0.18,0.1,0.1)x', results)
+
+# print(results[:,:,:,0])
 print('ANIMATING RESULTS')
-af.animate(results, save='testfig2.mp4')
+af.animate(results[:,:,50,:]) # Cut through
+af.animate(results[:,:,:,0])  # Normal view
