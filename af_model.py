e7f9b22ca9eb0eb50e7e33f3e0f7488c67627205
Unknown <andrewjford@hotmail.co.uk>
2017-10-11 14:21:55 +0100

    restructured files to add viewer
    Docstrings

--- af_model.py
+++ af_model.py
@@ -3,42 +3,62 @@ Model of atrial fibrillation
 
 Andrew Ford
 """
-import time
 
-import matplotlib.animation as animation
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
+
 import numpy as np
 
 
 class Substrate:
-    def __init__(self, substrate_size, structural_homogeneity,
-                 dysfunction_parameter, dysfunction_probability, refractory_period, layer_homogenity, seed=False):
+    """
+
+    """
+    def __init__(self, substrate_size, s_structural_homogeneity, p_structural_homogeneity,
+                 dysfunction_parameter, dysfunction_probability, refractory_period, seed=False):
+        """
+        Initialise a heart substrare.
+
+        :param substrate_size:
+        :param s_structural_homogeneity:
+        :param p_structural_homogeneity:
+        :param dysfunction_parameter:
+        :param dysfunction_probability:
+        :param refractory_period:
+        :param seed:
+        """
         if not seed:
             seed = np.random.randint(0,2**32-1, dtype='uint32')
         self.seed = seed
         self.r = np.random.RandomState(seed)
         self.substrate_size = substrate_size
-        self.structural_heterogeneity = structural_homogeneity
+        self.s_structural_heterogeneity = s_structural_homogeneity
+        self.p_structural_homogeneity = p_structural_homogeneity
         self.dysfunction_parameter = dysfunction_parameter
         self.dysfunction_probability = dysfunction_probability
         self.refractory_period = np.int8(refractory_period)
-        self.layer_homogenity = layer_homogenity
         self.activation = np.zeros(substrate_size, dtype='int8')  # Grid of activation state
-        self.linkage = self.r.choice(a=[True, False], size=substrate_size,  # Grid of downward linkages
-                                        p=[structural_homogeneity, 1 - structural_homogeneity])
-        self.layer_linkage = self.r.choice(a=[True, False], size=substrate_size,  # Grid of layer linkages
-                                        p=[layer_homogenity, 1 - layer_homogenity])
+        self.s_linkage = self.r.choice(a=[True, False], size=substrate_size,  # Grid of downward linkages
+                                        p=[s_structural_homogeneity, 1 - s_structural_homogeneity])
+        self.p_linkage = self.r.choice(a=[True, False], size=substrate_size,  # Grid of layer linkages
+                                        p=[p_structural_homogeneity, 1 - p_structural_homogeneity])
         self.dysfunctional = self.r.choice(a=[True, False], size=substrate_size,  # Grid of dysfunctional nodes
                                               p=[dysfunction_parameter, 1 - dysfunction_parameter])
 
         self.inactive = np.zeros(substrate_size, dtype=bool)  # Grid of currently dysfunctional nodes
 
     def activate_pacemaker(self):
-        # Activate the substrate pacemaker cells
+        """
+        Activate the substrate pacemaker cells
+
+        :return:
+        """
         self.activation[:, 0,:] = self.refractory_period
 
     def iterate(self):
+        """
+        Iterate substrate forward one time step and return activation matrix.
+
+        :return:
+        """
         excited = self.activation == self.refractory_period  # Condition for being excited
         resting = self.activation == 0  # Condition for resting
 
@@ -48,15 +68,15 @@ class Substrate:
         excited_from_fwrd = np.roll(excited, -1, axis=1)
         excited_from_fwrd[:, -1] = np.bool_(False)
 
-        excited_from_inside = np.roll(excited & self.layer_linkage, 1, axis=2)
+        excited_from_inside = np.roll(excited & self.p_linkage, 1, axis=2)
         excited_from_inside[:,:,0] = np.bool(False)
 
-        excited_from_outside = np.roll(excited & np.roll(self.layer_linkage, 1, axis=2), -1, axis=2)
+        excited_from_outside = np.roll(excited & np.roll(self.p_linkage, 1, axis=2), -1, axis=2)
         excited_from_outside[:,:,-1] = np.bool(False)
 
-        excited_from_above = np.roll(excited & self.linkage, 1, axis=0)
+        excited_from_above = np.roll(excited & self.s_linkage, 1, axis=0)
 
-        excited_from_below = np.roll(excited & np.roll(self.linkage, 1, axis=0), -1, axis=0)
+        excited_from_below = np.roll(excited & np.roll(self.s_linkage, 1, axis=0), -1, axis=0)
 
         excitable = (excited_from_rear | excited_from_fwrd | excited_from_above |
                      excited_from_below | excited_from_inside | excited_from_outside)
@@ -70,41 +90,20 @@ class Substrate:
         return self.activation
 
     def identifier(self):
-        return '{},{},{},{},{},{},{}'.format(self.substrate_size, self.structural_heterogeneity,self.dysfunction_parameter,
-                                    self.dysfunction_probability, self.refractory_period, self.layer_homogenity,
-                                    self.seed)
+        """
+        Retrun unique identifier for each substrate
+
+        :return:
+        """
+        return '{},{},{},{},{},{},{}'.format(self.substrate_size, self.s_structural_heterogeneity, self.dysfunction_parameter,
+                                             self.dysfunction_probability, self.refractory_period, self.p_structural_homogeneity,
+                                             self.seed)
 
 
-def simulation(runtime, pacemaker_period, substrate):
-    result = np.zeros((runtime,) + substrate.substrate_size, dtype='int8')
-    for t in range(runtime):
-        if t % pacemaker_period == 0:
-            substrate.activate_pacemaker()
-
-        result[t] = substrate.iterate()
-    return result
 
 
-def animate(results, save=False, cross_view=False, cross_pos=-1):
-    fig = plt.figure()
-    if cross_view:
-        gs = gridspec.GridSpec(1, 2, width_ratios=np.shape(results)[-2:])
-        ax1 = plt.subplot(gs[0])
-        ax2 = plt.subplot(gs[1])
-        ims = [[ax1.imshow(frame[:,:,0], animated=True), ax2.imshow(frame[:, cross_pos, :], animated=True)] for frame in results]
-    else:
-        ims = [[plt.imshow(frame[:,:,0], animated=True)] for frame in results]
-    ani = animation.ArtistAnimation(fig, ims, interval=20, blit=True,
-                                    repeat_delay=500)
-    if save:
-        plt.rcParams['animation.ffmpeg_path'] = "C:/Program Files/ffmpeg-20170807-1bef008-win64-static/bin/ffmpeg.exe"
 
-        writer = animation.writers['ffmpeg'](fps=30)
-        print('SAVING')
-        t = time.time()
-        ani.save(save, writer)
-        print('Saved as {} in {:.1f} seconds'.format(save, time.time()-t))
-    plt.show()
+
 
 
 
--- main.py
+++ main.py
@@ -1,26 +1,49 @@
 import time
 import af_model as af
+import viewer
 import numpy as np
 
-structural_homogeneity = .14  # Probability of transverse connections
-dysfunction_parameter = .05  # Fraction of dysfunctional cells
-dysfunction_probability = .05
-substrate_size = (200, 200, 10)
+
+def simulation(runtime, pacemaker_period, substrate):
+    """
+
+    :param runtime: Number of timesteps in simulation.
+    :type runtime: int
+    :param pacemaker_period: Period of the pacemaker cells
+    :type pacemaker_period: int
+    :param substrate: Substrate to run simulaton on
+    :type substrate: af_model.Substrate
+    :return:
+    :rtype:
+    """
+    result = np.zeros((runtime,) + substrate.substrate_size, dtype='int8')
+    for t in range(runtime):
+        if t % pacemaker_period == 0:
+            substrate.activate_pacemaker()
+        result[t] = substrate.iterate()
+    return result
+
+
+s_structural_homogeneity = .1  # Probability of transverse connections
+p_structural_homogeneity = .01
+dysfunction_parameter = 0  # Fraction of dysfunctional cells
+dysfunction_probability = 0
+substrate_size = (100, 100, 10)
 pacemaker_period = 220  # pacemaker activation period
 refractory_period = 50
 runtime = 1000
-layer_linkage = .05
+seed = False
 
 start = time.time()
 print('GENERATING SUBSTRATE')
 
-substrate = af.Substrate(substrate_size, structural_homogeneity,
-                         dysfunction_parameter, dysfunction_probability, refractory_period, layer_linkage)
+substrate = af.Substrate(substrate_size, s_structural_homogeneity, p_structural_homogeneity,
+                         dysfunction_parameter, dysfunction_probability, refractory_period, seed)
 print(substrate.identifier())
 
 print('RUNNING SIMULATION')
 
-results = af.simulation(runtime, pacemaker_period, substrate)
+results = simulation(runtime, pacemaker_period, substrate)
 
 runtime = time.time() - start
 print('SIMULATION COMPLETE IN {:.1f} SECONDS'.format(runtime))
@@ -29,4 +52,4 @@ print('SIMULATION COMPLETE IN {:.1f} SECONDS'.format(runtime))
 
 
 print('ANIMATING RESULTS')
-af.animate(results, cross_view=True, cross_pos=-50) # Cut through
+viewer.animate(results, refractory_period, cross_view=True, cross_pos=80)  # Cut through
