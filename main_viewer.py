dc8b44ae11a96bd4e7b8738111a8579fbb2f0449
Anthony Li <ant385@hotmail.co.uk>
2017-10-31 16:14:30 +0000

    Viewer attempt using matplotlib
    - Save function is so slow not worth it (screen capture would be better)
    - Try customise animation

--- main_viewer.py
+++ main_viewer.py
@@ -3,4 +3,4 @@ from viewer import Viewer
 model_viewer = Viewer('10-30_16-49-50')
 
 # model_viewer.plot_model_stats()
-model_viewer.view_model_array()
+model_viewer.animate_model_array()
--- viewer.py
+++ viewer.py
@@ -1,10 +1,13 @@
 import h5py
 import numpy as np
 import os
-import pyqtgraph as pg
 import seaborn as sns
 
 from matplotlib import pyplot as plt
+plt.rcParams['animation.ffmpeg_path'] = "C:/Year4_Project/ffmpeg-20170807-1bef008-win64-static/bin/ffmpeg.exe"
+from matplotlib import animation  # must be defined after defining ffmpeg line
+from matplotlib import gridspec
+from time import time
 
 
 class Viewer:
@@ -18,29 +21,6 @@ class Viewer:
         :param path: The path for data to read and view
         """
         self.path = path
-
-    def view_model_array(self):
-        """Read the HDF5 data file and view the model array."""
-
-        print("reading & viewing model array...")
-
-        # Import the model_array from HFD5 format
-        with h5py.File('data/{}/data_files/model_array_list'.format(self.path), 'r') as model_data_file:
-            model_array_list = model_data_file['array_list'][:]
-
-        app = pg.mkQApp()
-        view = pg.GraphicsLayoutWidget()
-        view.show()
-        w1 = view.addPlot()
-        w1.disableAutoRange()
-        for array in model_array_list:
-            w1.plot(array)
-        w1.autoRange()
-        app.exec_()
-
-        # if len(np.shape(model_array_list[0])) == 2:
-
-        # TODO: CHECK 2D VS 3D
 
     def plot_model_stats(self):
         """Read the HDF5 data file and plot the model statistics."""
@@ -94,8 +74,12 @@ class Viewer:
             plt.savefig(os.path.join('data', self.path, 'model_statistics', data_dict['filename']))
             plt.close()
 
-    def plot_model_array(self):
-        """Read the HDF5 data file and view the model array."""
+    def plot_model_array(self, time_steps=None, start=0):
+        """
+        Read the HDF5 data file and view the model array for a range of time steps.
+        :param time_steps: Number of time steps to plot
+        :param start: Start time
+        """
 
         print("reading & plotting model array...")
 
@@ -105,6 +89,72 @@ class Viewer:
 
         # TODO: CHECK 2D VS 3D
         # TODO: (output video - only way is matplotlib or screencap/snapshots (specify timestep) here)
+
+        # def plot_world_data(self, days=None, start=0, plot_world=False):
+        #     """
+        #     Plot the data for a range of times.
+        #     :param days: Number of days to plot
+        #     :param start: Start time
+        #     :param plot_world: Set to True to plot the world
+        #     """
+        #
+        #     # Counts number of CSV (comma-separated values) data files, equivalent to the total number of days simulated
+        #     total_days = len(
+        #         fnmatch.filter(os.listdir(os.path.join('data', self.seed, 'data_files', 'world_data')), '*.csv'))
+        #
+        #     if days is None or days > total_days:
+        #         days = total_days - start
+        #
+        #     # Plot the data for each day
+        #     for i in range(days):
+        #         sys.stdout.write(
+        #             '\r' + 'reading & plotting world data, time: %r' % (start + i) + '/%r' % (total_days - 1) + '...')
+        #         sys.stdout.flush()
+        #         self.plot_day_data(day=start + i, world=plot_world)
+
+    def animate_model_array(self, save=True):
+        """Read the HDF5 data file and animate the model array."""
+
+        print("reading & animating model array...")
+
+        # Import the model_array from HFD5 format
+        with h5py.File('data/{}/data_files/model_array_list'.format(self.path), 'r') as model_data_file:
+            model_array_list = model_data_file['array_list'][:]
+
+        refractory_period = max(model_array_list.flatten())
+
+        # if len(np.shape(model_array_list[0])) == 3:
+
+        fig = plt.figure()
+        ims = [[plt.imshow(frame[:, :, 0], animated=True, cmap='Greys_r', vmin=0, vmax=refractory_period)]
+               for frame in model_array_list]
+        ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=500)
+
+        plt.show()
+
+        if save:
+            if not os.path.exists(os.path.join('data', self.path, 'model_array')):
+                os.makedirs(os.path.join('data', self.path, 'model_array'))
+
+            writer = animation.writers['ffmpeg'](fps=30)
+            print("Saving animation (approx 1s/10 time steps for 40000 cells)...")
+            t = time()
+            ani.save(os.path.join('data', self.path, 'model_array', '_animation.mp4'), writer)
+            print("Saved in {:.1f}s".format(save, time() - t))
+
+        # TODO: 2D VS 3D
+        # TODO: ANIMATE A SPECIFIC TIME SEGMENT
+
+            # if cross_view:
+            #     gs = gridspec.GridSpec(1, 2, width_ratios=np.shape(results)[-2:])
+            #     ax1 = plt.subplot(gs[0])
+            #     ax2 = plt.subplot(gs[1])
+            #     ims = [[ax1.imshow(frame[:, :, 0], animated=True, vmin=0, vmax=refractory_period),
+            #             ax2.imshow(frame[:, cross_pos, :], animated=True, vmin=0, vmax=refractory_period),
+            #             ax1.axvline(x=cross_pos, color='cyan', zorder=10, animated=True, linestyle='--')]
+            #            for frame in results]
+            #
+            # else:
 
     def plot_ecg(self):
         """Read the HDF5 data file create an ECG from the model array."""
