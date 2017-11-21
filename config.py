ed917c557b7223c97db2ece994000784b73922ca
Anthony Li <ant385@hotmail.co.uk>
2017-11-21 14:34:10 +0000

    Quick Viewer Fix

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 settings = dict(
     structure=dict(
-        size=(1, 200, 200),  # (z, y, x)
+        size=(10, 200, 200),  # (z, y, x)
         refractory_period=50,
         dysfunction_parameter=0.05,  # fraction of dysfunctional cells
         dysfunction_probability=0.05,
-        x_coupling=1.,
-        y_coupling=.2,  # probability of y linkage
-        z_coupling=.1,
+        x_coupling=.542,
+        y_coupling=.228,  # probability of y linkage
+        z_coupling=.228,
         seed=None  # 11-13_17-02-49 pretty, .61,.61
     ),
     sim=dict(
