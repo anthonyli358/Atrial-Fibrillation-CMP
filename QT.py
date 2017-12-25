f1bd284ad051fc84ebe7e824da2510662ae5af7b
Unknown <andrewjford@hotmail.co.uk>
2017-12-25 21:38:47 +0000

    Added pause play , refresh and single step
    BUG:  Step broken after refresh

--- QT.py
+++ QT.py
@@ -16,15 +16,18 @@ import main
 import config
 
 class AFInterface(QtWidgets.QMainWindow):
+    """Atrial Fibrillation simulation class."""
     def __init__(self):
         super().__init__()
-
+        self.settings = config.settings
         self.initUI()
 
     def initUI(self):
 
-        self.anim = Animation()
-        #
+        self.settings['play'] = True
+        self.settings['step'] = False
+        self.settings['view'] = 'activation'
+        self.anim = Animation(self)
         self.setCentralWidget(self.anim)
         self.setWindowIcon(QtGui.QIcon('icons8-heart-with-pulse-50.png'))
 
@@ -42,14 +45,18 @@ class AFInterface(QtWidgets.QMainWindow):
         self.toolbar = self.addToolBar('Play')
         playAct = QtWidgets.QAction(QtGui.QIcon('Icons/icons8-play-50.png'), 'play', self)
         playAct.setCheckable(True)
-        playAct.setChecked(True)
+        playAct.setChecked(self.settings['play'])
         playAct.triggered.connect(self.toggle_pause)
         playAct.setShortcut('space')
         chartAct = QtWidgets.QAction(QtGui.QIcon('Icons/icons8-heat-map-50.png'), 'chart', self)
         chartAct.triggered.connect(self.show_phase)
-        settAct = QtWidgets.QAction(QtGui.QIcon('Icons/icons8-services-50.png'), 'settings', self)
+        settAct = QtWidgets.QAction(QtGui.QIcon('Icons/icons8-settings-50.png'), 'settings', self)
         settAct.triggered.connect(self.show_config)
-        self.toolbar.addActions([playAct, chartAct, settAct])
+        resetAct = QtWidgets.QAction(QtGui.QIcon('Icons/icons8-reset-50.png'), 'reset', self)
+        resetAct.triggered.connect(self.reset)
+        advAct = QtWidgets.QAction(QtGui.QIcon('Icons/icons8-advance-50.png'), 'advance', self)
+        advAct.triggered.connect(self.advance)
+        self.toolbar.addActions([playAct, chartAct, settAct, resetAct, advAct])
         self.toolbar.setMaximumHeight(25)
 
 
@@ -67,69 +74,183 @@ class AFInterface(QtWidgets.QMainWindow):
 
     def toggle_pause(self):
 
-        config.settings['pause'] ^= True
+        self.settings['play'] ^= True  # Toggle play setting
 
     def show_config(self):
-        self.popup = Config()
+        self.popup = Config(self)
         self.popup.setWindowTitle('Config')
         self.popup.show()
 
     def show_phase(self):
-        self.popup = makePhases()
+        self.popup = makePhases(self)
         self.popup.setWindowTitle('Phase Spaces')
         self.popup.show()
+
+    def reset(self):
+        if not config.settings['structure']['seed']:
+            self.settings['structure']['seed'] = None
+        self.anim = Animation(self)
+        self.setCentralWidget(self.anim)
+
+    def advance(self):
+        self.settings['step'] = True
 
 
 
 
 class Config(QtWidgets.QWidget):
-    def __init__(self):
+    """Edit system settings"""
+    def __init__(self, parent):
         super().__init__()
-        form = QtWidgets.QFormLayout()
-        self.setWindowIcon(QtGui.QIcon('icons8-services-50.png'))
-        self.sizeq= QtWidgets.QLineEdit(str(config.settings['structure']['size']))
-        # self.sizeq.editingFinished.connect(self.printer)
-        form.addRow(QtWidgets.QLabel('Dimensions'), self.sizeq)
+        self.parent = parent
+        self.settings = parent.settings
+        self.setWindowIcon(QtGui.QIcon('icons8-settings-50.png'))
+        self.sizeq= QtWidgets.QLineEdit(str(self.settings['structure']['size']))
+        self.setWindowTitle('Configuration Settings')
+        self.initUI()
 
-        form.addRow(QtWidgets.QLabel('Linkage'), QtWidgets.QLineEdit())
-        refrt = QtWidgets.QLineEdit('220')
-        form.addRow(QtWidgets.QLabel('Refractory Period'), refrt)
+    def initUI(self):
 
+        config_form = QtWidgets.QFormLayout()
+
+        x_dim = QtWidgets.QSpinBox()
+        y_dim = QtWidgets.QSpinBox()
+        z_dim = QtWidgets.QSpinBox()
+        x_dim.setRange(1,999)
+        y_dim.setRange(1,999)
+        z_dim.setRange(1,999)
+        x_dim.setValue(self.settings['structure']['size'][2])
+        y_dim.setValue(self.settings['structure']['size'][1])
+        z_dim.setValue(self.settings['structure']['size'][0])
+        x_dim.valueChanged.connect(self.update_x_dim)
+        y_dim.valueChanged.connect(self.update_y_dim)
+        z_dim.valueChanged.connect(self.update_z_dim)
+        xyz_dim = QtWidgets.QHBoxLayout()
+        xyz_dim.addWidget(QtWidgets.QLabel('x:'))
+        xyz_dim.addWidget(x_dim)
+        xyz_dim.addWidget(QtWidgets.QLabel('y:'))
+        xyz_dim.addWidget(y_dim)
+        xyz_dim.addWidget(QtWidgets.QLabel('z:'))
+        xyz_dim.addWidget(z_dim)
+
+        config_form.addRow(QtWidgets.QLabel('Dimensions'), xyz_dim)
+
+        x_linkage = QtWidgets.QDoubleSpinBox()
+        y_linkage = QtWidgets.QDoubleSpinBox()
+        z_linkage = QtWidgets.QDoubleSpinBox()
+        x_linkage.setDecimals(3)
+        y_linkage.setDecimals(3)
+        z_linkage.setDecimals(3)
+        x_linkage.setRange(0.00, 1.00)
+        y_linkage.setRange(0, 1)
+        z_linkage.setRange(0, 1)
+        x_linkage.setSingleStep(0.01)
+        y_linkage.setSingleStep(0.01)
+        z_linkage.setSingleStep(0.01)
+        x_linkage.setValue(self.settings['structure']['x_coupling'])
+        y_linkage.setValue(self.settings['structure']['y_coupling'])
+        z_linkage.setValue(self.settings['structure']['z_coupling'])
+        x_linkage.valueChanged.connect(self.update_x_linkage)
+        y_linkage.valueChanged.connect(self.update_y_linkage)
+        z_linkage.valueChanged.connect(self.update_z_linkage)
+
+        xyz_linkage = QtWidgets.QHBoxLayout()
+        xyz_linkage.addWidget(QtWidgets.QLabel('x:'))
+        xyz_linkage.addWidget(x_linkage)
+        xyz_linkage.addWidget(QtWidgets.QLabel('y:'))
+        xyz_linkage.addWidget(y_linkage)
+        xyz_linkage.addWidget(QtWidgets.QLabel('z:'))
+        xyz_linkage.addWidget(z_linkage)
+
+        config_form.addRow(QtWidgets.QLabel('Linkage'), xyz_linkage)
+
+        refractoryBox = QtWidgets.QSpinBox()
+        refractoryBox.setRange(1, 999)
+        refractoryBox.setValue(self.settings['structure']['refractory_period'])
+        refractoryBox.valueChanged.connect(self.update_refractory)
+        config_form.addRow(QtWidgets.QLabel('Refractory Period'), refractoryBox)
+
+        reset_button = QtWidgets.QPushButton()
+        reset_button.setText('Reset with these settings')
+        reset_button.pressed.connect(self.parent.reset)
+
+        config_form.addWidget(reset_button)
+
+        config_box = QtWidgets.QGroupBox()
+        config_box.setTitle('Substrate Configuration Settings')
+        config_box.setLayout(config_form)
+
+        anim_form = QtWidgets.QFormLayout()
         viewopts = QtWidgets.QComboBox()
         viewopts.addItems(['activation', 'count'])
         viewopts.currentTextChanged.connect(self.updateview)
-        form.addRow(QtWidgets.QLabel('Animation Style'), viewopts)
+        anim_form.addRow(QtWidgets.QLabel('Animation Style'), viewopts)
 
         crosspos_slider = QtWidgets.QSlider(Qt.Horizontal)
-        crosspos_slider.setValue(50)
-        crosspos_slider.setRange(0, config.settings['structure']['size'][1]-1)
         crosspos_spin = QtWidgets.QSpinBox()
-        crosspos_spin.setValue(config.settings['crosspos'])
-        crosspos_spin.setRange(0, config.settings['structure']['size'][1]-1)
 
-        crosspos_spin.valueChanged.connect(crosspos_slider.setValue)
-        crosspos_slider.valueChanged.connect(crosspos_spin.setValue)
+        crosspos_slider.setValue(self.settings['crosspos'])
+        crosspos_spin.setValue(self.settings['crosspos'])
+        crosspos_slider.setRange(0, self.settings['structure']['size'][1]-1)
+        crosspos_spin.setRange(0, self.settings['structure']['size'][1]-1)
+        crosspos_slider.valueChanged.connect(crosspos_spin.setValue)  # Connect slider to spin boc
+        crosspos_spin.valueChanged.connect(crosspos_slider.setValue)  # Connect spin box to slider
+        crosspos_slider.valueChanged.connect(self.updatecrosspos)  # Connect slider to position updater
+        crosspos_spin.valueChanged.connect(self.updatecrosspos)  # Connect spin box to position updater
 
-        crosspos_spin.valueChanged.connect(self.updatecrosspos)
         crosspos = QtWidgets.QHBoxLayout()
         crosspos.addWidget(crosspos_spin)
         crosspos.addWidget(crosspos_slider)
-        form.addRow(QtWidgets.QLabel('Cross view position'), crosspos)
+        anim_form.addRow(QtWidgets.QLabel('Cross view position'), crosspos)
 
-        self.setLayout(form)
-        self.setWindowTitle('Configuration Settings')
+        anim_box = QtWidgets.QGroupBox()
+        anim_box.setTitle('Animation Settings')
+        anim_box.setLayout(anim_form)
+
+        box_list = QtWidgets.QVBoxLayout()
+
+        box_list.addWidget(config_box)
+        box_list.addWidget(anim_box)
+
+        self.setLayout(box_list)
+
+    # ToDo: Change slider range when size is changed
+
+    def update_x_dim(self, val):
+        self.settings['structure']['size'][2] = val
+
+    def update_y_dim(self, val):
+        self.settings['structure']['size'][1] = val
+
+    def update_z_dim(self, val):
+        self.settings['structure']['size'][0] = val
+
+    def update_x_linkage(self, val):
+        self.settings['structure']['x_coupling'] = val
+
+    def update_y_linkage(self, val):
+        self.settings['structure']['y_coupling'] = val
+
+    def update_z_linkage(self, val):
+        self.settings['structure']['z_coupling'] = val
+
+    def update_refractory(self, val):
+        self.settings['structure']['refractory_period'] = val
+
 
     def updateview(self, val):
-        config.settings['view'] = val
+        self.settings['view'] = val
 
     def updatecrosspos(self, val):
-        config.settings['crosspos'] = val
+        self.settings['crosspos'] = val
 
 
 class makeCanvas(FigureCanvas):
-    def __init__(self):
+    """Parent class of all Canvases. Initiate a figure with a toolbar."""
+    def __init__(self, parent):
         self.figure = Figure(dpi=50)
         super().__init__(self.figure)
+        self.settings = parent.settings
         self.compute_initial_figure()
 
         self.toolbar = NavigationToolbar(self, self)
@@ -140,9 +261,11 @@ class makeCanvas(FigureCanvas):
 
 
 class makePhases(makeCanvas):
+    """Window containing figures of risk for different substrates."""
 
     def compute_initial_figure(self):
         self.resize(400,350)
+        self.step = False
         names = ['Phase_Spaces/1_200_200.npy', 'Phase_Spaces/2_200_200.npy',
                  'Phase_Spaces/4_200_200.npy', 'Phase_Spaces/8_200_200.npy',
                  'Phase_Spaces/16_200_200.npy', 'Phase_Spaces/32_200_200.npy']
@@ -157,56 +280,90 @@ class makePhases(makeCanvas):
 
 
 class Animation(makeCanvas):
-
+    """Window with real-time Atrial Fibrillation animation."""
     def compute_initial_figure(self):
-        self.substrate = model.Model(**config.settings['structure'])
+        self.substrate = model.Model(**self.settings['structure'])
 
-        gs = GridSpec(1, 2, width_ratios=config.settings['structure']['size'][-2::-1])
+        gs = GridSpec(1, 2, width_ratios=self.settings['structure']['size'][-2::-1])
 
         self.ax1 = self.figure.add_subplot(gs[0])
-        line = self.ax1.axvline(x=config.settings['crosspos'], color='cyan', linestyle='--')
-        image = self.ax1.imshow(self.substrate.model_array[0], animated=True, cmap='Oranges_r',
-                                vmin=0, vmax=config.settings['structure']['refractory_period'],
-                                origin = 'lower', alpha=1, extent=(0, 200, 0, 200), interpolation='nearest',
-                                zorder=1)
-        image2 = self.ax1.imshow(self.substrate.model_array[-1], animated=True, cmap='Greys_r',
-                                 vmin=0, vmax=config.settings['structure']['refractory_period'],
-                                 origin = 'lower', alpha = 0.5, extent=(0, 200, 0, 200), interpolation='nearest',
-                                 zorder=1)
+        line = self.ax1.axvline(x=self.settings['crosspos'],
+                                color='cyan',
+                                linestyle='--'
+                                )
+        image = self.ax1.imshow(self.substrate.model_array[0],
+                                animated=True,
+                                cmap='Oranges_r',
+                                vmin=0,
+                                vmax=self.settings['structure']['refractory_period'],
+                                origin = 'lower',
+                                alpha=1,
+                                extent=(0, self.settings['structure']['size'][2],
+                                        0, self.settings['structure']['size'][1]),
+                                interpolation='nearest',
+                                zorder=1,
+                                )
+        image2 = self.ax1.imshow(self.substrate.model_array[-1],
+                                 animated=True,
+                                 cmap='Greys_r',
+                                 vmin=0,
+                                 vmax=self.settings['structure']['refractory_period'],
+                                 origin = 'lower',
+                                 alpha = 0.5,
+                                 extent=(0, self.settings['structure']['size'][2],
+                                         0, self.settings['structure']['size'][1]),
+                                 interpolation='nearest',
+                                 zorder=1
+
+                                 )
 
         self.ax2 = self.figure.add_subplot(gs[1])
-        crossim = self.ax2.imshow(np.swapaxes(self.substrate.model_array[:,:,config.settings['crosspos']], 0,1), animated=True,
-                                  vmin=0, vmax=config.settings['structure']['refractory_period'],
-                                  origin = 'lower', cmap='Greys_r', interpolation='nearest')
+        crossim = self.ax2.imshow(np.swapaxes(self.substrate.model_array[:,:,self.settings['crosspos']], 0,1),
+                                  animated=True,
+                                  vmin=0,
+                                  vmax=self.settings['structure']['refractory_period'],
+                                  origin='lower',
+                                  cmap='Greys_r',
+                                  interpolation='nearest',
+                                  extent=(0, self.settings['structure']['size'][0],
+                                          0, self.settings['structure']['size'][1])
+                                  )
 
         def func(framedata):
-            t, pause = framedata
+            t, play = framedata
             """Function to iterate animation over"""
-            if t % config.settings['sim']['pacemaker_period'] == 0:
+            if t % self.settings['sim']['pacemaker_period'] == 0:
                 self.substrate.activate_pacemaker()
             self.ax1.set_title('seed={}, t={}'.format(self.substrate.seed, t))
-            if not pause:
+            if play:
                 self.substrate.iterate()
             arr = self.get_anim_array()
-            return [line.set_xdata(config.settings['crosspos']), image.set_data(arr[0]), image2.set_data(arr[-1]),
-                    crossim.set_data(np.swapaxes(arr[:,:,config.settings['crosspos']], 0,1)),
+            return [line.set_xdata(self.settings['crosspos']),
+                    image.set_data(arr[0]),
+                    image2.set_data(arr[-1]),
+                    crossim.set_data(np.swapaxes(arr[:,:,self.settings['crosspos']], 0,1)),
                     ]
 
         def frames():
             t = -1
             while True:
-                if not config.settings['pause']:
+                advanced = False
+                if self.settings['play'] or self.settings['step']:
+                    self.settings['step'] = False  # enable one timestep only while paused
+                    advanced = True  # let substrate iterate
                     t += 1
-                yield (t, config.settings['pause'])
+                yield (t, advanced)
 
         self.ani = FuncAnimation(self.figure, func, frames, interval=1, blit=False)
 
     def get_anim_array(self):
-        method = config.settings['view']
+        method = self.settings['view']
         if method == 'activation':
             return self.substrate.model_array
         if method == 'count':
-            return self.substrate.excount % 3 / 3 * config.settings['structure']['refractory_period']
+            return self.substrate.excount % 3 / 3 * self.settings['structure']['refractory_period']
+
+
 
 
 
--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 settings = dict(
     structure=dict(
-        size=(16, 200, 200),  # (z, y, x)
+        size=[16, 200, 200],  # (z, y, x)
         refractory_period=50,
         dysfunction_parameter=0.05,  # fraction of dysfunctional cells
         dysfunction_probability=0.05,
-        x_coupling=.97,
+        x_coupling=.9,
         y_coupling=.1,  # probability of y linkage
         z_coupling=.1,
         seed=None # 11-13_17-02-49 pretty, .61,.61
@@ -19,7 +19,5 @@ settings = dict(
         cross_pos=50,
         interval=1,  # length of each frame in milliseconds
     ),
-    view='activation',
     crosspos=50,
-    pause=False
 )
