2b163be00c7b5ecf4d7f17e18902ec5e47230545
Unknown <andrewjford@hotmail.co.uk>
2018-06-07 14:38:05 +0100

    Added variable connectivities
    nu_x int now  [min,max] etc
    y and z merged to yz_connectivities
    Removed qt support for changing  connectivities, will be readded later

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 settings = dict(
     structure=dict(
-        size=[25, 200, 200],  # (z, y, x)
+        size=[25, 200, 600],  # (z, y, x)
         refractory_period=50,
         dysfunction_parameter=0.05,  # fraction of dysfunctional cells
         dysfunction_probability=0.05,
-        x_coupling=.9,
-        y_coupling=.08,  # probability of y linkage
-        z_coupling=.08,
+        x_coupling=[0,1],
+        yz_coupling=[.5,.5],  # probability of y linkage
+
         seed=None # 11-13_17-02-49 pretty, .61,.61
     ),
     sim=dict(
-        pacemaker_period=220,  # pacemaker activation period
+        pacemaker_period=60,  # pacemaker activation period
         runtime=1000,
     ),
     viewer=dict(
--- model.py
+++ model.py
@@ -7,13 +7,12 @@ class Model:
     """
 
     def __init__(self, size, refractory_period, dysfunction_parameter, dysfunction_probability, x_coupling,
-                 y_coupling, z_coupling, seed, time=0):
+                 yz_coupling, seed, time=0):
         """
         Heart Initialisation
         :param size: The dimensions of the heart as a tuple e.g. (200, 200, 10)
         :param x_coupling: The x yz_coupling factor
-        :param y_coupling: The y yz_coupling factor
-        :param z_coupling: The z yz_coupling factor
+        :param yz_coupling: The y yz_coupling factor
         :param dysfunction_parameter: The fraction of dysfunctional cells
         :param dysfunction_probability: The fraction of dysfunctional cells which fail to excite
         :param refractory_period: Cell refractory period
@@ -25,9 +24,8 @@ class Model:
         self.dysfunction_parameter = dysfunction_parameter
         self.dysfunction_probability = dysfunction_probability
         self.time = time
-        self.seed = seed if seed is not None else datetime.datetime.now().strftime('%m-%d_%H-%M-%S')
+        self.seed = seed if seed is not None else np.random.randint(np.iinfo('uint32').max, dtype='uint32')
         self.direction = np.zeros(size, dtype='uint8')
-        np.random.seed(int(sub('[^0-9]', '', self.seed)))
 
         self.excited = np.zeros(size, dtype=bool)
         self.resting = np.ones(size, dtype=bool)
@@ -35,12 +33,24 @@ class Model:
         self._max = 0
         self.maxpos = [0, 0, 0]
         self.model_array = np.zeros(size, dtype='uint8')  # array of model_array state
-        self.x_linkage = np.random.choice(a=[True, False], size=size,  # array of longitudinal linkages
-                                          p=[x_coupling, 1 - x_coupling])
-        self.y_linkage = np.random.choice(a=[True, False], size=size,  # array of transverse linkages
-                                          p=[y_coupling, 1 - y_coupling])
-        self.z_linkage = np.random.choice(a=[True, False], size=size,  # array of layer linkages
-                                          p=[z_coupling, 1 - z_coupling])
+
+        x_coupling_grid = np.linspace(x_coupling[0],x_coupling[1], size[0])
+        yz_coupling_grid = np.linspace(yz_coupling[0], yz_coupling[1], size[0])
+
+        x_ran = np.random.random(size)
+        y_ran = np.random.random(size)
+        z_ran = np.random.random(size)
+
+        self.x_linkage = np.apply_along_axis(np.less, 0, x_ran, x_coupling_grid)
+        self.y_linkage = np.apply_along_axis(np.less, 0, y_ran, yz_coupling_grid)
+        self.z_linkage = np.apply_along_axis(np.less, 0, z_ran, yz_coupling_grid)
+
+        # self.x_linkage = np.random.choice(a=[True, False], size=size,  # array of longitudinal linkages
+        #                                   p=[x_coupling, 1 - x_coupling])
+        # self.y_linkage = np.random.choice(a=[True, False], size=size,  # array of transverse linkages
+        #                                   p=[yz_coupling, 1 - yz_coupling])
+        # self.z_linkage = np.random.choice(a=[True, False], size=size,  # array of layer linkages
+        #                                   p=[yz_coupling, 1 - yz_coupling])
         self.x_linkage[:, :, -1] = False  # No links from end
         self.z_linkage[-1, :, :] = False
         self.dysfunctional = np.random.choice(a=[True, False], size=size,  # array of dysfunctional nodes
