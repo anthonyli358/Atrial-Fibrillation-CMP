8fcd6da85d9c214f0ebb4b5880271e4f80e2813b
Unknown <ant385@hotmail.co.uk>
2018-06-13 20:12:55 +0100

    Full risk curve
    - Mean AF Time (not sure about AF definition here)
    - AF Induced
    - Can plot from multiple data files

--- plot_risk_curve_data.py
+++ plot_risk_curve_data.py
@@ -1,4 +1,16 @@
 import h5py
+import numpy as np
 
-with h5py.File('dat_analysis/new_risk_homogeneous/'.format(self.path), 'r') as model_data_file:
-    model_array_list = model_data_file['array_list'][:]
+
+path = "new_risk_homogeneous/l_z=1, nu_x=1.0, nu_yz=0"
+
+af_risk = []
+for l_z in [1]:
+    for nu_x in [1.0]:
+        for nu_yz in [0.1, 0.2]:
+            with h5py.File('data_analysis/new_risk_homogeneous/l_z={}, nu_x={}, nu_yz={}'.format(l_z, nu_x, nu_yz),
+                           'r') as data_file:
+                risk_data = data_file['risk'][:]
+            af_risk.append([nu_x, nu_yz, np.sum(risk_data[:, :, 1]) / len(risk_data[:, :, 1])])
+
+print(af_risk)
