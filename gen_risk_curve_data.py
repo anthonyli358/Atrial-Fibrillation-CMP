eb0203e3d83e4953ec834eee742e6504f4f70313
Unknown <ant385@hotmail.co.uk>
2018-06-14 11:53:39 +0100

    Risk Curve generator & plotter
    - working generator
    - plot from the range of files
    - added folders to gitignore

--- gen_risk_curve_data.py
+++ gen_risk_curve_data.py
@@ -6,66 +6,69 @@ import numpy as np
 import time
 
 
-def risk_curve_data(runs, repeats, l_z, nu_x, nu_yz, angle_vars=False, time=100000):
+def risk_curve_data(runs, repeats, l_z, nu_x, nu_yz, angle_vars=False, t=100000):
     data = np.zeros(shape=(runs, repeats, 3), dtype='uint32')
     params = config.settings["structure"]
     params["size"][0], params["seed"] = l_z, None
     params["x_coupling"], params["yz_coupling"] = nu_x, nu_yz
     params["angle_toggle"], params["angle_vars"] = angle_vars, angle_vars
+    print("l_z: {}, nu_x: {}, nu_yz: {}, angle_vars: {}".format(l_z, nu_x, nu_yz, angle_vars))
+    start = time.time()
     for i in range(runs):
         tissue = Model(**params)
         for j in range(repeats):
             tissue.model_array.fill(0)  # Clear activations
+            tissue.excount.fill(0)  # Clear excitation count
+            tissue.activate_pacemaker()  # Initialise new wavefront
             tissue.time = 0
             data[i, j, 0] = tissue.seed
-            ended = False
-            while tissue.time < time:
-                if tissue.time % 1000 == 0:
-                    tissue.excount.fill(0)  # Clear excitation count
-                    tissue.activate_pacemaker()
+            while tissue.time < t:
                 excitations = tissue.iterate()
                 if np.intersect1d(tissue.excount, [2]):
                     maxpos = tissue.maxpos
                     data[i, j, 1] = 1
                     data[i, j, 2] = maxpos[0] * 100000 + maxpos[1] * 1000 + maxpos[2]
                     break
-                elif ended is False:
-                    if not np.any(excitations == 50):
-                        data[i, j, 2] = tissue.time + 1
-                        ended = True
-            print('Run: {}, Repeat: {}, Data: {}'.format(i, j, data[i][j]))
+                elif not np.any(excitations == 50):
+                    data[i, j, 2] = tissue.time
+                    break
+            print('Run: {}, Repeat: {}, Data: {}'.format(i + 1, j + 1, data[i][j]))
+    print("time={}".format(time.time() - start))
 
     return data
 
 
-def af_time_data(runs, repeats, l_z, nu_x, nu_yz, angle_vars=False, time=100000):
+def af_time_data(runs, repeats, l_z, nu_x, nu_yz, angle_vars=False, t=100000):
     data = np.zeros(shape=(runs, repeats, 3), dtype='uint32')
     params = config.settings["structure"]
     params["size"][0], params["seed"] = l_z, None
     params["x_coupling"], params["yz_coupling"] = nu_x, nu_yz
     params["angle_toggle"], params["angle_vars"] = angle_vars, angle_vars
+    print("l_z: {}, nu_x: {}, nu_yz: {}, angle_vars: {}".format(l_z, nu_x, nu_yz, angle_vars))
+    start = time.time()
     for i in range(runs):
         tissue = Model(**params)
         for j in range(repeats):
             tissue.model_array.fill(0)  # Clear activations
             tissue.excount.fill(0)
             tissue.time = 0
-            data[i][j][0] = tissue.seed
+            data[i, j, 0] = tissue.seed
             fib_time = 0
-            while tissue.time < time:
-                if tissue.time % 200 == 0:
+            while tissue.time < t:
+                if tissue.time % 220 == 0:
                     tissue.activate_pacemaker()
                 tissue.iterate()
                 if tissue.maxpos[2] > 1:
-                    data[i][j][1] = 1  # Fibrillation
+                    data[i, j, 1] = 1  # Fibrillation
                     fib_time += 1
-            data[i][j][2] = fib_time
-            print('Run: {}, Repeat: {}, Data: {}'.format(i, j, data[i][j]))
+            data[i, j, 2] = fib_time
+            print('Run: {}, Repeat: {}, Data: {}'.format(i + 1, j + 1, data[i][j]))
+    print("time={}".format(time.time() - start))
 
     return data
 
 
-def gen_risk(runs, repeats, l_z, nu_x_range, nu_yz_range, angle_vars=False, time=100000, time_data=False):
+def gen_risk(runs, repeats, l_z, nu_x_range, nu_yz_range, angle_vars=False, t=100000, time_data=False):
     risk_type = af_time_data if time_data else risk_curve_data
     dir_name = "new_risk_angled" if angle_vars else "new_risk_homogeneous"
     if not os.path.exists('data_analysis/{}'.format(dir_name)):
@@ -73,21 +76,22 @@ def gen_risk(runs, repeats, l_z, nu_x_range, nu_yz_range, angle_vars=False, time
 
     if angle_vars:
         for av in angle_vars[2]:
-            result = risk_type(runs, repeats, l_z, 1.0, 1.0, [angle_vars[0], angle_vars[1], av], time)
+            result = risk_type(runs, repeats, l_z, 1.0, 1.0, [angle_vars[0], angle_vars[1], av], t)
             with h5py.File('data_analysis/{}/{}, l_z={}, ang_epi={}, ang_endo={}, nu_av={}'.format(
                     dir_name, risk_type.__name__, l_z, angle_vars[0], angle_vars[1], av), 'w') as data_file:
                 data_file.create_dataset('risk', data=result, dtype='uint32')
     else:
         for x in nu_x_range:
             for yz in nu_yz_range:
-                result = risk_type(runs, repeats, l_z, x, yz, False, time)
+                result = risk_type(runs, repeats, l_z, x, yz, False, t)
                 with h5py.File('data_analysis/{}/{}, l_z={}, nu_x={}, nu_yz={}'.format(
                         dir_name, risk_type.__name__, l_z, x, yz), 'w') as data_file:
                     data_file.create_dataset('risk', data=result, dtype='uint32')
 
 
 if __name__ == '__main__':
-    for i in [1, 5, 25]:
-        start = time.time()
-        gen_risk(runs=20, repeats=1, l_z=i, nu_x_range=[1.0], nu_yz_range=[0.2], angle_vars=False, time=5000)
-        print(time.time() - start)
+    for i in [1, 5]:
+        gen_risk(runs=100, repeats=2, l_z=i, nu_x_range=[0.1, 0.3, 0.5, 0.7, 0.9], nu_yz_range=[0.1, 0.3, 0.5, 0.7, 0.9],
+                 angle_vars=False, t=100000, time_data=False)
+        gen_risk(runs=50, repeats=2, l_z=i, nu_x_range=[0.1, 0.3, 0.5, 0.7, 0.9], nu_yz_range=[0.1, 0.3, 0.5, 0.7, 0.9],
+                 angle_vars=False, t=100000, time_data=True)
--- plot_risk_curve_data.py
+++ plot_risk_curve_data.py
@@ -6,10 +6,10 @@ path = "new_risk_homogeneous/l_z=1, nu_x=1.0, nu_yz=0"
 
 af_risk = []
 for l_z in [1]:
-    for nu_x in [1.0]:
-        for nu_yz in [0.1, 0.2]:
-            with h5py.File('data_analysis/new_risk_homogeneous/l_z={}, nu_x={}, nu_yz={}'.format(l_z, nu_x, nu_yz),
-                           'r') as data_file:
+    for nu_x in [0.8, 0.9, 1.0]:
+        for nu_yz in [0.4]:
+            with h5py.File('data_analysis/new_risk_homogeneous/risk_curve_data, l_z={}, nu_x={}, nu_yz={}'.format(
+                    l_z, nu_x, nu_yz), 'r') as data_file:
                 risk_data = data_file['risk'][:]
             af_risk.append([nu_x, nu_yz, np.sum(risk_data[:, :, 1]) / len(risk_data[:, :, 1])])
 
