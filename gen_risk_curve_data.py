707e8d0797f7ca9ace0e27737b0ad1d2ed657d79
Unknown <andrewjford@hotmail.co.uk>
2018-06-15 16:17:13 +0100

    added system params

--- gen_risk_curve_data.py
+++ gen_risk_curve_data.py
@@ -4,7 +4,7 @@ from utility_methods import *
 import numpy as np
 import time
 import binascii
-
+import sys
 
 
 def risk_curve_data(runs, l_z, nu_x, nu_yz, angle_vars=False, t=100000):
@@ -94,22 +94,21 @@ def gen_risk(runs, l_z, nu_x, nu_yz, angle_vars=False, t=100000, time_data=False
 
     np.save(filename, result)
 
-
 if __name__ == '__main__':
-    input_value=5
+
     # change the variables, you can loop over nu_x and nu_y
-    param_space = np.load('nu_variables_low_res.npy')
-    print
-    for [x,y] in param_space[:100]:
-        variables = dict(
-            runs=1,
-            l_z=25,
-            nu_x=x,
-            nu_yz=y,
-            # to loop over various angles, do angle_vars=[ang_zmin, ang_zmax, nu_av], looping over nu_av
-            # if angle_vars are defined nu_x, nu_y are ignored (angular fibre simulation)
-            angle_vars=False,
-            t=10000,
-            time_data=False,  # True for AF time sim, False for AF induction probability sim
-        )
-        gen_risk(**variables)
+    input_value = int(sys.argv[1])
+    [x,y] = np.load('nu_variables_low_res.npy')[input_value]
+
+    variables = dict(
+        runs=1,
+        l_z=25,
+        nu_x=x,
+        nu_yz=y,
+        # to loop over various angles, do angle_vars=[ang_zmin, ang_zmax, nu_av], looping over nu_av
+        # if angle_vars are defined nu_x, nu_y are ignored (angular fibre simulation)
+        angle_vars=False,
+        t=10000,
+        time_data=False,  # True for AF time sim, False for AF induction probability sim
+    )
+    gen_risk(**variables)
--- plot_risk_curve_data.py
+++ plot_risk_curve_data.py
@@ -8,6 +8,7 @@ nu_yz_val = np.arange(0.1, 1, 0.02)
 nu_x_val = np.arange(0.1, 1, 0.02)
 X,Y = np.meshgrid(nu_x_val,nu_yz_val)
 Z = np.zeros(X.shape)
+con = np.zeros_like(Z)
 wabwab = np.zeros(X.shape)
 
 example = 'risk_curve_data_5_0100_0500_False_10000_eb8610e35046e63f'
@@ -23,6 +24,8 @@ for yi in range(len(nu_yz_val)):
             # print(filename)
             risk_data = np.load(filename)
             Z[xi,yi] = np.sum(risk_data[:, 1]) / risk_data[:, 1].size
+            con[xi,yi] = np.sum(risk_data[:, -1]) / risk_data[:, -1].size
+
         except:
             pass
         if 0.36 * x * x - 0.79 * x + 0.25 <= y <= 0.375 * x * x - 0.825 * x + 0.65:
@@ -31,4 +34,6 @@ for yi in range(len(nu_yz_val)):
 # res = 0.36*X*X - 0.79*X +0.43 <= Y <= 0.375*X*X - 0.825*X +0.65
 plt.imshow(Z, extent=(0,1,0,1), origin='lower')
 plt.imshow(wabwab, extent=(0,1,0,1), origin='lower', alpha=.1,zorder=2)
+# plt.figure()
+plt.imshow(con, extent=(0,1,0,1) ,cmap='Blues', origin='lower', alpha = 0.3,zorder=3)
 plt.show()
