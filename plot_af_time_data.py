c084ae8214f01a70b371344b6c6db234089b5144
Unknown <andrewjford@hotmail.co.uk>
2018-06-19 15:48:33 +0100

    af_plotting

--- plot_af_time_data.py
+++ plot_af_time_data.py
@@ -3,20 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 
-nu_yz_val = np.arange(0.005, 1, 0.01)
-nu_x_val = np.arange(0.005, 1, 0.01)
+(nu_x_val, nu_yz_val) = np.load('nu_variables_exact_1821.npy')[90:110].transpose()
+# nu_yz_val = np.arange(0.005, 1, 0.01)
+# nu_x_val = np.arange(0.005, 1, 0.01)
 X, Y = np.meshgrid(nu_x_val, nu_yz_val)
 Z = np.zeros(X.shape)
 time = np.zeros_like(Z)
-var = np.zeros_like(Z)
-wabwab = np.zeros(X.shape)
-avz = np.zeros_like(Z)
-varz = np.zeros_like(Z)
-end_time = np.zeros_like(Z)
-conduction_block = np.zeros_like(Z)
-non_zero_coordinates = np.zeros_like(Z)
-mask = np.load('test.npy')
-new_mask = []
+num = np.zeros_like(Z)
+
 
 
 
@@ -25,37 +19,33 @@ for yi in range(len(nu_yz_val)):
         x = X[xi, yi]
         y = Y[xi, yi]
         try:
-            start = 'af_time_data_25_0050_0410_False_10000_283924f4ac25c83f.npy'
-            start = 'afinduced_data/risk_curve_data_1000_25_{:.3f}_{:.3f}*'.format(x, y).replace('.', '')
+            start = 'af_time_data_25_5_{:.3f}_{:.3f}*'.format(x, y).replace('.', '')
             filename = glob.glob(start)[0]
             # print(filename)
-            risk_data = np.load(filename)
-            ave = np.average(risk_data[:, 1])
-            var[yi, xi] = np.std(risk_data[:, 1])
-            fib = risk_data[risk_data[:, 1] == 1]
-            no_fib = risk_data[(risk_data[:, 1] == 0) & (risk_data[:, -1] == 1)]
-            time[yi, xi] = np.average(fib[:, 5])
-            end_time[yi, xi] = np.average(no_fib[:, 5])
-            normz = (np.absolute((fib[:, 2]).astype('int') - 12))
-            avz[yi, xi] = np.average(normz)
-            varz[yi, xi] = np.std(normz)
-            conduction_block[yi, xi] = np.average(risk_data[:, -1])
+            risk_data = np.load(filename).tolist()
+            time[yi,xi] = np.average([i[1] for i in risk_data])
+            num[yi,xi] = np.average([len(i)/2 -1 for i in risk_data])
+            # ave = np.average(risk_data[:, 1])
+            # var[yi, xi] = np.std(risk_data[:, 1])
+            # fib = risk_data[risk_data[:, 1] == 1]
+            # no_fib = risk_data[(risk_data[:, 1] == 0) & (risk_data[:, -1] == 1)]
+            # time[yi, xi] = np.average(fib[:, 5])
+            # end_time[yi, xi] = np.average(no_fib[:, 5])
+            # normz = (np.absolute((fib[:, 2]).astype('int') - 12))
+            # avz[yi, xi] = np.average(normz)
+            # varz[yi, xi] = np.std(normz)
+            # conduction_block[yi, xi] = np.average(risk_data[:, -1])
 
 
         except:
             ave = 0
             pass
+
         Z[yi, xi] = ave - .5 * (ave == 0)
-        # if ave:
-        #     new_mask.append([x,y])
-        #     non_zero_coordinates[yi, xi] = True
-        if [x, y] in mask:
-            non_zero_coordinates[yi, xi] = True
-        # if 0.36 * x * x - 0.79 * x + 0.25 <= y <= 0.375 * x * x - 0.825 * x + 0.65:
-        #     wabwab[xi,yi] = True
+
 
 # res = 0.36*X*X - 0.79*X +0.43 <= Y <= 0.375*X*X - 0.825*X +0.65
-plt.imshow(Z, extent=(0, 1, 0, 1), origin='lower', zorder=1)
+plt.imshow(time, extent=(0, 1, 0, 1), origin='lower', zorder=1)
 plt.title('Risk curve')
 # plt.contour(X,Y,Z, levels=[.1,.9], colors='w')
 grid_args = dict(color='w',
@@ -75,24 +65,10 @@ plt.ylim((0,1))
 
 
 plt.figure()
-plt.imshow(time, extent=(0, 1, 0, 1), origin='lower', zorder=3)
-plt.title('Time to fibrillation')
+plt.imshow(num, extent=(0, 1, 0, 1), origin='lower', zorder=3)
+plt.title('Number of circuit restarts')
 plt.clim((100., 220.))
 # plt.imshow(wabwab, extent=(0,1,0,1), origin='lower', alpha=.1,zorder=2)
-plt.figure()
-plt.imshow(avz, extent=(0, 1, 0, 1), origin='lower', vmin=5, vmax=10)
-plt.title('Averaege z')
 
-plt.figure()
-plt.imshow(conduction_block, extent=(0, 1, 0, 1), origin='lower')
-plt.title('Conduction Block')
-
-plt.figure()
-plt.imshow(200 /end_time , extent=(0, 1, 0, 1), origin='lower')
-plt.title('conduction speed')
-
-plt.figure()
-plt.imshow(non_zero_coordinates, extent=(0, 1, 0, 1), origin='lower')
-plt.title('Mask')
 
 plt.show()
